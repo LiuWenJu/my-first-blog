32b0b54dc6ed74cf125890e9ea73be15a06b3980
LiuWenJu <lwj037303@gmail.com>
2016-12-30 17:24:28 +0800

    secure blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jnj)k-8w&h5ymi@y#61a7sek@u+5q&rrv_63$et3gwiugf!ly('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost:8000', '.pythonanywhere.com']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
